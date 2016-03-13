65b930093e57d9c56cbb60e31421cb14151da8d3
Ivaylo Arnaudov <ivaylo.arnaudov12@gmail.com>
2016-03-13 02:20:20 +0200

    Integrate only the required theme parts to the asset pipeline

--- hacklet/config/initializers/assets.rb
+++ hacklet/config/initializers/assets.rb
@@ -1,11 +1,22 @@
-# Be sure to restart your server when you modify this file.
-
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
 
-# Add additional assets to the asset load path
-# Rails.application.config.assets.paths << Emoji.images_path
-
-# Precompile additional assets.
-# application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-# Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile +=
+  ['bootstrap/bootstrap.min.css',
+   'smarty/essentials.css',
+   'smarty/layout.css',
+   'smarty/header-1.css',
+   'smarty/color-scheme/blue.css',
+   'jquery/jquery-2.1.4.min.js',
+   'smarty/scripts.js',
+   'smarty/contact.js',
+   '*.png',
+   '*.jpg',
+   '*.jpeg',
+   '*.gif',
+   '*.svg',
+   '*.eot',
+   '*.woff',
+   '*.woff2',
+   '*.ttf'
+   ]
