675d23d967f8577cfc6e9dc4b2942d125340787b
Plamen Neshkov <plamenneshkov@ghostmail.com>
2016-03-22 18:23:10 +0200

    List attendances in profile page

--- hacklet/app/models/user.rb
+++ hacklet/app/models/user.rb
@@ -6,6 +6,7 @@ class User < ActiveRecord::Base
   has_many :teams, through: :participations
   has_many :invitations, :class_name => 'Invite', :foreign_key => 'recipient_id'
   has_many :sent_invites, :class_name => 'Invite', :foreign_key => 'sender_id'
+  has_many :attendances
 
   devise :database_authenticatable, :registerable, :recoverable,
     :rememberable, :trackable, :validatable, :confirmable,
